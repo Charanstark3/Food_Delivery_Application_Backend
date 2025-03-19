26db834c3ad0b9429f63d1f68a55e7643f3bdc3e
Chandramouli M <130632952+Charanstark3@users.noreply.github.com>
2025-03-19 13:10:11 +0530

    env revtify

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const dotenv = require("dotenv").config()
 const products = require("./routes/productRoutes");
 const restaurants = require("./routes/restaurantRoutes");
 
-const port = process.env.PORT
+const port = process.env.PORT||3000;
 
 app.use(express.json()); //used for parsing so that always the data return in json
 app.use("/api", products);
